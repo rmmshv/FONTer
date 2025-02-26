03a27587fdd613b06bd53a89ec727d21f265441d
rimma <32555704+rmmshv@users.noreply.github.com>
2025-02-26 01:41:46 -0800

    added links to the fonts

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,68 +1,91 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { FaHeart } from "react-icons/fa";
 import fitty from 'fitty';
 import './Body.css';
 import { folors } from "../data/data";
+import { FaExternalLinkAlt, FaLink } from 'react-icons/fa';
 
 const Body = () => {
-  const { fontsAndColors, secondRow, thirdRow, randLorem } = folors;
+  const { fontsAndColors } = folors;
 
-  // Refs for each row type
+  // State to manage custom text input for each font card
+  const [customTexts, setCustomTexts] = useState(
+    fontsAndColors.map(() => ({ firstRow: '', secondRow: '', thirdRow: '', randLorem: '' , link: ''}))
+  );
+
+  // Refs for fitty to adjust font sizes dynamically
   const firstRowRefs = useRef([]);
   const secondRowRefs = useRef([]);
   const thirdRowRefs = useRef([]);
   const randLoremRefs = useRef([]);
-
- 
+  
 
   useEffect(() => {
-    // Apply fitty to each set of text elements
-    firstRowRefs.current.forEach(el => fitty(el, { maxSize: 200 }));
-    secondRowRefs.current.forEach(el => fitty(el, { maxSize: 150 }));
-    thirdRowRefs.current.forEach(el => fitty(el, { maxSize: 100 }));
-    randLoremRefs.current.forEach(el => fitty(el, { maxSize: 50 }));
-  }, []);
+    firstRowRefs.current.forEach(el => el && fitty(el, { maxSize: 200 }));
+    secondRowRefs.current.forEach(el => el && fitty(el, { maxSize: 150 }));
+    thirdRowRefs.current.forEach(el => el && fitty(el, { maxSize: 100 }));
+    randLoremRefs.current.forEach(el => el && fitty(el, { maxSize: 50 }));
+  }, [customTexts]);
+
+  // Handle text change
+  const handleTextChange = (index, field, value) => {
+    const newCustomTexts = [...customTexts];
+    newCustomTexts[index][field] = value;
+    setCustomTexts(newCustomTexts);
+  };
 
   return (
     <section className='folors' id='folors'>
-      <h2>{secondRow}</h2>
-      <h3>{thirdRow}</h3>
-      <p>{randLorem}</p>
       <div className='container-grid'>
         {fontsAndColors.map((folor, index) => (
           <article className='folor-card' key={index}>
-            <h1 
-              className='firstRow' 
-              ref={el => firstRowRefs.current[index] = el}
+            {/* First Row */}
+            <h1
+              className='firstRow'
+              ref={el => (firstRowRefs.current[index] = el)}
               style={{ fontFamily: folor.fontName, fontStyle: folor.fontStyle }}
             >
-              {folor.firstRow}
+              {customTexts[index].firstRow || folor.firstRow}
             </h1>
-            <h2 
-              className='secondRow' 
-              ref={el => secondRowRefs.current[index] = el}
+            
+
+            {/* Second Row */}
+            <h2
+              className='secondRow'
+              ref={el => (secondRowRefs.current[index] = el)}
               style={{ fontFamily: folor.fontName, fontStyle: folor.fontStyle }}
             >
-              {folor.secondRow}
+              {customTexts[index].secondRow || folor.secondRow}
             </h2>
-            <h3 
-              className='thirdRow' 
-              ref={el => thirdRowRefs.current[index] = el}
+        
+
+            {/* Third Row */}
+            <h3
+              className='thirdRow'
+              ref={el => (thirdRowRefs.current[index] = el)}
               style={{ fontFamily: folor.fontName, fontStyle: folor.fontStyle }}
             >
-              {folor.thirdRow}
+              {customTexts[index].thirdRow || folor.thirdRow}
             </h3>
+           
+
+            {/* Random Lorem */}
             <p
               className='randLorem'
-              ref={el => randLoremRefs.current[index] = el}
+              ref={el => (randLoremRefs.current[index] = el)}
               style={{ fontFamily: folor.fontName, fontStyle: folor.fontStyle }}
             >
-               {folor.randLorem} 
+              {customTexts[index].randLorem || folor.randLorem}
             </p>
+            
+
             {/* Bottom Info Section */}
             <div className="bottom-info">
+            <a href={folor.link} title="Go to the font" target='_blank' rel="noreferrer">
+                <FaExternalLinkAlt className='icon' size={18}/>
+              </a>
               <p className="font-name">Font: {folor.fontName}</p>
               <p className="font-style">Style: {folor.fontStyle}</p>
+              
             </div>
             
           </article>
--- src/data/data.js
+++ src/data/data.js
@@ -8,105 +8,138 @@ const about = {
 
 const fontsAndColorsData = [
     ["EIGHT", "MANICULE", "SOFT HYPHEN", "Rollergirls", "Regular", 
-     "Rare, tail, lend, episode, century, when, muffin, vehicle, shuffle, drum, doll, crisp"],
+     "Rare, tail, lend, episode, century, when, muffin, vehicle, shuffle, drum, doll, crisp",
+    "https://www.dafont.com/rollergirls.font"],
 
     ["DOLLARS", "EUROS", "POUNDS", "Porscha", "Regular", 
-     "Pottery, spell, praise, chronic-glance, settle, wine, sun, awake, picture, warm, erase"],
+     "Pottery, spell, praise, chronic-glance, settle, wine, sun, awake, picture, warm, erase",
+    "https://www.dafont.com/911porscha.font"],
 
     ["SQUID", "DRUIDS", "PROPER RARER ROAR", "GrandParadiso", "Regular", 
-     "Clown, half, easy, good, paper, thrive, arena, modify, velvet, luxury, daring, certain"],
+     "Clown, half, easy, good, paper, thrive, arena, modify, velvet, luxury, daring, certain",
+    "https://www.dafont.com/grand-paradiso.font"],
 
     ["BALLER", "GRAVITY", "BERRY BRACKET", "FatKat", "Regular", 
-     "Frequent, doctor, promote, shy, draft, dilemma, ripple, casino, pottery, prosper, auction, provide"],
+     "Frequent, doctor, promote, shy, draft, dilemma, ripple, casino, pottery, prosper, auction, provide",
+    "https://www.dafont.com/fat-kat.font"],
 
     ["PUNK", "PARENTHESIS", "TILDE", "JapanRich", "Bold", 
-     "Recycle, crumble, solve, unable, flat, brass, chicken, husband, mosquito, pave, breeze, midnight"],
+     "Recycle, crumble, solve, unable, flat, brass, chicken, husband, mosquito, pave, breeze, midnight",
+    "https://www.dafont.com/japan-rich.font"],
 
     ["I AM", "GROOT", "I AM I AM I AM", "Narnia", "Italic", 
-     "Glass, nerve, kite, exile, water, chronic, nation, media, cabbage, damp, govern, regular"],
+     "Glass, nerve, kite, exile, water, chronic, nation, media, cabbage, damp, govern, regular",
+    "https://www.dafont.com/narnia.font"],
 
     ["NINE", "BECAUSE", "SPLENDID YES CHARM", "Neon", "Regular", 
-     "Empty, fire, loyal, nation, unit, shadow, brown, night, vehicle, powder, hint, oppose"],
+     "Empty, fire, loyal, nation, unit, shadow, brown, night, vehicle, powder, hint, oppose",
+    "https://www.dafont.com/neon-80s.font"],
 
     ["STEAK", "BULL", "APOSTROPHE", "HeavyHeap", "Regular", 
-     "Lorizzle, ipsizzle, dolizzle, sit-amizzle, brizzle, elizzle"],
+     "Lorizzle, ipsizzle, dolizzle, sit-amizzle, brizzle, elizzle",
+    "https://www.dafont.com/heavy-heap.font"],
 
     ["QUEUE", "MARQUEE", "LONGING MUG BLISS", "Sunwish", "Regular", 
-     "Silent, dune, whisper, eye, strategy, remember, globe, hole, innocent, cute, lounge, brave"],
+     "Silent, dune, whisper, eye, strategy, remember, globe, hole, innocent, cute, lounge, brave",
+    "https://www.dafont.com/sunwish-maverick.font"],
 
     ["ACUTE", "INDEED", "ASTROPHYSICS", "Lemonmilk", "Regular", 
-     "Food, draft, forest, also, onion, doctor, scene, silk, scrap, scorpion, depth, want"],
+     "Food, draft, forest, also, onion, doctor, scene, silk, scrap, scorpion, depth, want",
+    "https://www.dafont.com/lemon-milk.font"],
 
-    ["WHO", "FIVE", "DOCTOR WOLF ROSE", "pinkoyablack", "Regular", 
-     "Defy, nose, slight, network, copy, decide, creek, grid, brass, burst, real, pair"],
+    ["WHO", "FIVE", "DOCTOR WOLF ROSE", "pincoyablack", "Regular", 
+     "Defy, nose, slight, network, copy, decide, creek, grid, brass, burst, real, pair",
+    "https://www.dafont.com/pincoyablack.font"],
 
     ["BEAR", "ENQUIRY", "JAGUAR JUNGLE", "Highrise", "Italic", 
-     "Diesel, bag, chapter, screen, infant, soon, settle, victory, two, tomato, detail, faith"],
+     "Diesel, bag, chapter, screen, infant, soon, settle, victory, two, tomato, detail, faith",
+    "https://www.dafont.com/highrise.font"],
 
     ["DASH", "CRATORS", "ROCK N ROLL", "Coiny", "Regular", 
-     "Crisp, file, web, predict, fiction, access, diamond, earn, panda, train, gather, draft"],
+     "Crisp, file, web, predict, fiction, access, diamond, earn, panda, train, gather, draft",
+    "https://www.dafont.com/coiny.font"],
 
     ["TAB", "INDEX", "OXFORD COMMA", "Astonpoliz", "Regular", 
-     "Circle, adapt, lizard, genre, layer, curious, labor, radar, ginger, file, tray, arctic"],
+     "Circle, adapt, lizard, genre, layer, curious, labor, radar, ginger, file, tray, arctic",
+    "https://www.dafont.com/astonpoliz.font"],
 
     ["HASH", "PROGRAM", "BEEPING BUZZ", "Bayside", "Regular", 
-     "Cave, squirrel, welcome, buddy, alter, shallow, joy, science, shoot, when, action, record"],
+     "Cave, squirrel, welcome, buddy, alter, shallow, joy, science, shoot, when, action, record",
+    "https://www.dafont.com/bayside.font"],
 
     ["YEN", "LEOPARD", "README CACHE", "Bulagria", "Regular", 
-     "Gentle, bargain, gesture, during, opinion, bachelor, avoid, forward, kid, river, rapid, panel"],
+     "Gentle, bargain, gesture, during, opinion, bachelor, avoid, forward, kid, river, rapid, panel",
+    "https://www.dafont.com/bulagria.font"],
 
     ["ZONE", "POOF", "MICROCHIP", "Cinematografica", "Regular", 
-     "Collect, witness, chat, pitch, venture, viable, bring, health, sand, vicious, honey, recall"],
+     "Collect, witness, chat, pitch, venture, viable, bring, health, sand, vicious, honey, recall",
+    "https://www.dafont.com/cinematografica.font"],
 
-    ["DOLLA", "CONDUCTOR", "ROOT HOME CD", "DKlounge", "Italic", 
-     "Ghost, quote, kitten, urban, gadget, brick, have, exotic, creek, horn, normal, mammal"],
+    ["DOLLA", "CONDUCTOR", "ROOT HOME CD", "DKlongreach", "Italic", 
+     "Ghost, quote, kitten, urban, gadget, brick, have, exotic, creek, horn, normal, mammal",
+    "https://www.dafont.com/dk-longreach.font"],
 
-    ["THREE", "WOW", "KEY FACTOR SLASH", "EastmanRoman", "Regular", 
-     "Bullet, rail, flip, loyal, fire, symbol, jelly, wire, indoor, weasel, immune, drastic"],
+    ["THREE", "WOW", "KEY FACTOR SLASH", "Eastman", "Regular", 
+     "Bullet, rail, flip, loyal, fire, symbol, jelly, wire, indoor, weasel, immune, drastic",
+    "https://www.dafont.com/eastman.font"],
 
     ["POW", "FREE SOURCE", "AMPERSAND AND", "Evogria", "Regular", 
-     "Door, antenna, pipe, add, unit, fabric, tattoo, toe, join, cook, eyebrow, cheap"],
+     "Door, antenna, pipe, add, unit, fabric, tattoo, toe, join, cook, eyebrow, cheap",
+    "https://www.dafont.com/evogria.font"],
 
     ["YA!", "PLUS", "STEREO VAULT", "BankPrinter", "Regular", 
-     "Hungry, between, electric, task, result, monster, medal, iron, mystery, year, gym, skill"],
+     "Hungry, between, electric, task, result, monster, medal, iron, mystery, year, gym, skill",
+    "https://www.dafont.com/f25-bank-printer.font"],
 
     ["XEROX", "MERGING", "DROID BELL", "Glorify", "Regular", 
-     "Voyage, private, coil, display, interest, animal, rigid, convince, empty, garage, youth, present"],
+     "Voyage, private, coil, display, interest, animal, rigid, convince, empty, garage, youth, present",
+    "https://www.dafont.com/glorify-sh.font"],
 
     ["HAY", "OCTOTHORPE", "DITTO WAH", "Carbon", "Regular", 
-     "Hint, story, wasp, name, casual, someone, manage, sudden, bid, family, crystal, offer"],
+     "Hint, story, wasp, name, casual, someone, manage, sudden, bid, family, crystal, offer",
+    "https://www.dafont.com/carbon.font"],
 
     ["SIX", "METALLIC", "BREW", "Lovan", "Regular", 
-     "Banana, desert, category, mom, noodle, couple, beauty, jungle, master, index, certain"],
+     "Banana, desert, category, mom, noodle, couple, beauty, jungle, master, index, certain",
+    "https://www.dafont.com/lovan.font"],
 
     ["YARN", "CLOUDY", "GIT HERO", "Longhaul", "Regular", 
-     "Lucky, radio, magic, slight, card, frog, motion, gate, sand, region, hip, actor"],
+     "Lucky, radio, magic, slight, card, frog, motion, gate, sand, region, hip, actor",
+    "https://www.dafont.com/longhaul.font"],
 
-    ["WATER", "RETURN", "KNOW TAB CAPS", "pinkoyablack", "Regular", 
-     "Exhibit, action, upper, month, vast, where, hamster, crack, loop, legend, asset, punch"],
+    ["WATER", "RETURN", "KNOW TAB CAPS", "pincoyablack", "Regular", 
+     "Exhibit, action, upper, month, vast, where, hamster, crack, loop, legend, asset, punch",
+    "https://www.dafont.com/pincoyablack.font"],
 
     ["OPT", "HEDERA", "LAMA MAMA", "Notted", "Regular", 
-     "Primary, place, address, tower, response, phone, just, such, tag, anchor, net, happy"],
+     "Primary, place, address, tower, response, phone, just, such, tag, anchor, net, happy",
+     "https://www.dafont.com/notted.font",
+    ],
 
     ["COMMA", "ELLIPSIS", "ALF MEOW YUM", "RacingCar", "Regular", 
-     "Crisp, file, web, predict, fiction, access, diamond, earn, panda, train, gather, draft"],
+     "Crisp, file, web, predict, fiction, access, diamond, earn, panda, train, gather, draft",
+    "https://www.dafont.com/racing-car.font"],
 
     ["BOO!", "PERCENT", "OUCH", "Quicksilver", "Regular", 
-     "Survey, spray, together, shuffle, evoke, scrap, legend, rival, tunnel, entry, photo, wagon"],
+     "Survey, spray, together, shuffle, evoke, scrap, legend, rival, tunnel, entry, photo, wagon",
+    "https://www.dafont.com/quicksilver-3.font"],
 
     ["Psilograph", "RUN LINE", "DOT DOT DOT", "ButlerStencil", "Regular", 
-     "Enrich, uncle, guide, cradle, blind, record, across, maze, offer, couch, inspire, gorilla"],
+     "Enrich, uncle, guide, cradle, blind, record, across, maze, offer, couch, inspire, gorilla",
+    "https://www.dafont.com/butler-stencil.font"],
 
     ["ZAATAR", "SHAWARMA", "TOUM GARLIC", "LeJourSerif", "Regular", 
-     "Quality, security, genius, involve, cupboard, exist, ready, amused, patient, absorb, audit, dinner"],
+     "Quality, security, genius, involve, cupboard, exist, ready, amused, patient, absorb, audit, dinner",
+    "https://www.dafont.com/le-jour-serif.font"],
 
     ["AYRAN", "SHASHLIK", "COAL COAL COAL", "GrandMalefic", "Regular", 
-     "Prize, much, glide, outside, page, small, rigid, velvet, squeeze, brave, shuffle, grit"],
+     "Prize, much, glide, outside, page, small, rigid, velvet, squeeze, brave, shuffle, grit",
+    "https://www.dafont.com/grand-malefic.font"],
 ];
 
 const folors = {
-    fontsAndColors: fontsAndColorsData.map(([firstRow, secondRow, thirdRow, fontName, fontStyle, randLorem], index) => ({
-        id: index + 1, firstRow, secondRow, thirdRow, fontName, fontStyle, randLorem
+    fontsAndColors: fontsAndColorsData.map(([firstRow, secondRow, thirdRow, fontName, fontStyle, randLorem, link], index) => ({
+        id: index + 1, firstRow, secondRow, thirdRow, fontName, fontStyle, randLorem, link
     }))
 };
 
